225319ad50d209d4975a70d4c10c8d4c89c116bd
João Vitor S. Oliveira <j.vitor_oliveira1@hotmail.com>
2024-03-24 01:54:03 -0300

    refact: challenge 06

--- challenge_06/README.md
+++ challenge_06/README.md
@@ -15,7 +15,79 @@ A solução foi pensada e projetada em POO e a sua execução está no arquivo `
 
 A cada inserção, alteração ou conclusão de tarefas, a lista é ordenada baseada no status de conclusão e na prioridade. De qualquer forma, decidi deixar a opção no menu para que fique explícito que há essa funcionalidade.
 
-:warning: A cobertura de testes está reduzida nesse exercício pois o coverage não está excluindo o arquivo `main.py`, este que por sua vez possui métodos de interface com o usuário. Além disso, os prints da inferface com o usuário gerou 04 code smells no código, por conta de sua duplicidade.
 
-**No mais, os testes das classes Task e TaskList estão com alta cobertura.**
+## Como executar
 
+<details>
+<summary><strong>Em ambiente local</strong></summary></br>
+
+Crie o ambiente virtual (caso não tenha feito anteriormente)
+```bash
+python -m venv .venv
+```
+
+Ative o ambiente
+
+**LINUX e OS X**
+```bash
+source .venv/bin/activate
+```
+
+**WINDOWS**
+```bash
+\.venv\Scripts\activate
+```
+
+Instale as dependências
+```bash
+python -m pip install -r dev-requirements.txt
+```
+
+**Na raiz do projeto**
+
+Execute o script
+```bash
+python -m challenge_06.src.main
+```
+
+Execute os testes
+```bash
+python -m pytest -v
+```
+
+Execute a cobertura de testes
+```bash
+python -m pytest --cov
+```
+</details>
+
+<details>
+<summary><strong>Docker</strong></summary></br>
+
+**Certifique-se de possuir o docker e docker-compose instalados na sua máquina e com seus respectivos serviços ativados**
+
+Criando container
+```bash
+docker-compose up -d
+```
+
+Acessando o container
+```bash
+docker exec -it python-environment bash
+```
+
+Execute o script
+```bash
+python -m challenge_06.src.main
+```
+
+Execute os testes
+```bash
+python -m pytest -v
+```
+
+Execute a cobertura de testes
+```bash
+python -m pytest --cov
+```
+</details>
--- challenge_06/src/task_list.py
+++ challenge_06/src/task_list.py
@@ -4,38 +4,56 @@ from challenge_06.src.task import Task
 
 class TaskList:
 
-    def __init__(self):
+    def __init__(self) -> None:
+        '''Initializes a list of tasks.'''
         self.__tasks = []
 
-    def __str__(self):
+    def __str__(self) -> str:
+        '''Returns the representation of object as a string.'''
         result = ""
         for index, task in enumerate(self.__tasks):
             result = f"{index} - {task}\n"
         return result
 
-    def __len__(self):
+    def __len__(self) -> int:
+        '''Returns the number of tasks in the list.'''
         return len(self.__tasks)
 
-    def add_task(self, task: Task):
+    def add_task(self, task: Task) -> None:
+        '''Adds a task in the list.'''
         self.__tasks.append(task)
 
-    def update_task(self, index: int, description: str = None, priority: Priority = None):
+    def update_task(self, index: int, description: str = None, priority: Priority = None) -> None:
+        '''Updates the task in a given index given a new description and priority.'''
         self.__tasks[index].update(description, priority)
 
-    def complete_task(self, index: int):
+    def complete_task(self, index: int) -> None:
+        '''Marks the task as completed on given index.'''
         self.__tasks[index].complete()
 
-    def delete_task(self, index: int):
+    def delete_task(self, index: int) -> Task:
+        '''Deletes a task in a given index.'''
         del self.__tasks[index]
 
-    def get_tasks(self):
+    def get_tasks(self) -> list:
+        '''Return the list of tasks.'''
         return self.__tasks
 
-    def exists_task(self, index):
+    def exists_task(self, index: int) -> bool:
+        '''
+        Check if a tasks exists in a given index.
+
+        Args:
+            index (int): The index to be checked.
+
+        Returns:
+            bool: True if task exists, False otherwise.
+        '''
         if index >= 0 and index < len(self.__tasks):
             return True
         else:
             return False
 
-    def sort_by_status_and_priority(self):
+    def sort_by_status_and_priority(self) -> None:
+        '''Sorts the tasks by status and priority.'''
         self.__tasks.sort(key=lambda task: (task.status.value, task.priority.value))
